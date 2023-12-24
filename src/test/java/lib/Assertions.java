a6643dd1cf5589a1792bb881fb3f364401fbd252
ShevchenkoMaria <kuznecova1009@gmail.com>
2023-12-24 15:40:52 +0300

    Добавление/актуализация файлов в рамках блока лекций "IV. Создание фреймворка и запуск в Docker"

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -3,6 +3,7 @@ package lib;
 import io.restassured.response.Response;
 
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
@@ -10,5 +11,27 @@ public class Assertions {
         Response.then().assertThat().body("$", hasKey(name));
         int value = Response.jsonPath().getInt(name);
         assertEquals(expectedValue, value, "JSON value <"+value+"> is no equal to expected value <"+expectedValue+">");
+    }
+    public static void assertJsonByName(Response Response, String name, String expectedValue){
+        Response.then().assertThat().body("$", hasKey(name));
+        String value = Response.jsonPath().getString(name);
+        assertEquals(expectedValue, value, "JSON value <"+value+"> is no equal to expected value <"+expectedValue+">");
+    }
+    public static void assertResponseTextEquals(Response Response, String expectedAnswer){
+        assertEquals(expectedAnswer,Response.asString(),"Response test is not as expected");
+    }
+    public static void assertResponseCodeEquals(Response Response, int expectedStatusCode){
+        assertEquals(expectedStatusCode,Response.statusCode(),"Response status code is not as expected");
+    }
+    public static void assertJsonHasField(Response Response, String expectedKey){
+        Response.then().assertThat().body("$", hasKey(expectedKey));
+    }
+    public static void assertJsonHasFields(Response Response, String [] expectedFieldNames){
+        for (String expectedFieldName : expectedFieldNames){
+            Assertions.assertJsonHasField(Response, expectedFieldName);
+        }
+    }
+    public static void assertJsonHasNotField(Response Response, String unexpectedKey){
+        Response.then().assertThat().body("$", not(hasKey(unexpectedKey)));
     }
 }
